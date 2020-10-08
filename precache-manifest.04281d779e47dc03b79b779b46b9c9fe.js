7b3393927123c8b924ce9eac80afc8623fa01975
Yulia <yulia.scherbina@gmail.com>
2020-10-08 09:42:04 +0200

    Updates

---
+++ precache-manifest.04281d779e47dc03b79b779b46b9c9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037a00ca95401ede3a9939d5150de4e0",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "aaddaf0149b7ef8f044b",
+    "url": "/game-of-life/static/css/main.d8d55716.chunk.css"
+  },
+  {
+    "revision": "f440e7034ec7c905324e",
+    "url": "/game-of-life/static/js/2.6a10a10f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.6a10a10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaddaf0149b7ef8f044b",
+    "url": "/game-of-life/static/js/main.d6c1d925.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
